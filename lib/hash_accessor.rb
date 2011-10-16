885d8cb45717628e92a0aca5702033d427a83298
Forrest Zeisler <forrest@getjobber.com>
2011-10-15 21:48:35 -0700

    cleaning up layout

--- lib/hash_accessor.rb
+++ lib/hash_accessor.rb
@@ -1,12 +1,12 @@
 $:.unshift(File.dirname(__FILE__)) unless
 $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
-require 'railtie' if defined?(Rails)
+require 'hash_accessor/railtie' if defined?(Rails)
 
 module HashAccessor
   VERSION = "0.0.1"
   
-  autoload :ClassMethods, 'class_methods'  
+  autoload :ClassMethods, 'hash_accessor/class_methods'  
   
   def self.included(mod)
     mod.extend(ClassMethods)
---
+++ tests/test_hash_accessor.rb
@@ -0,0 +1,8 @@
+require 'test/unit'
+require 'hash_accessor'
+
+class HashAccessorTest < Test::Unit::TestCase
+  def test_english_hello
+    assert true
+  end
+end
